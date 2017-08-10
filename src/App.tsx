ea547aac6437e6cd6d694f68677a84a6b8c5455f
carney520 <carney520@hotmail.com>
2017-08-10 11:43:17 +0800

    支持相对src路径导入模块

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@
  * 根组件
  */
 import * as React from 'react'
+import Title from 'components/Title'
 
 export default class App extends React.Component {
   public render() {
-    return <h1>hello world!</h1>
+    return <Title>hello world!</Title>
   }
 }
---
+++ src/components/Title.tsx
@@ -0,0 +1,8 @@
+/**
+ * 标题
+ */
+import styled from 'styled-components'
+
+export default styled.h1`
+  color: red;
+`
